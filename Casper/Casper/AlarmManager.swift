9da0b3f8e92626446f867f76c352c4d78b9fc7c8
Ben Navetta <ben.navetta@gmail.com>
2015-10-31 20:22:29 -0400

    Start modeling distinct alerts for an alarm

--- Casper/Casper/AlarmManager.swift
+++ Casper/Casper/AlarmManager.swift
@@ -9,12 +9,33 @@
 import Foundation
 import UIKit
 
+struct Alert {
+    let timeBefore: NSTimeInterval // how long before the start date to start sending this alert
+    let interval: Int // time between notifications, in minutes
+    let soundName: String
+}
+
 struct Alarm {
     let time: NSDate
+    let alerts: Array<Alert>
 }
 
 class AlarmManager {
     static let sharedInstance = AlarmManager()
+    
+    private func createNotifications(alert: Alert, alarm: Alarm) {
+        for schedule in notificationSchedules(alarm.time.dateByAddingTimeInterval(-alert.timeBefore), interval: alert.interval) {
+            let notification = UILocalNotification()
+            notification.fireDate = schedule.fireDate
+            notification.timeZone = NSTimeZone.defaultTimeZone()
+            notification.alertTitle = "Wake Up"
+            notification.alertBody = "Casper wants you to get up!"
+            notification.category = "ALARM"
+            notification.repeatInterval = schedule.repeatInterval
+            notification.soundName = alert.soundName
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        }
+    }
     
     func schedule(alarm: Alarm) {
         let notification = UILocalNotification()
